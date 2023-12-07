1def98dee1731a208e8f19a48b6944c9db428026
Wajid Khan <mawk-khan@users.noreply.github.com>
2023-12-07 17:04:52 +0530

    Create functions.php

---
+++ functions.php
@@ -0,0 +1,12 @@
+/**
+ * Include the Custom ACF Year Picker Field Type
+ *
+ * This line includes the 'acf-year-picker.php' file, which contains the definition of a custom
+ * ACF field type named 'Year Picker'. The 'Year Picker' field type extends the functionality of
+ * Advanced Custom Fields (ACF) by adding a field that allows users to select a year from a dropdown.
+ * This is particularly useful for date-related data where only the year is relevant.
+ *
+ * The 'acf-year-picker.php' file defines the 'acf_field_year_picker' class, which handles the
+ * rendering, functionality, and script/style enqueuing for this custom field type.
+ */
+include_once('acf-year-picker.php');
